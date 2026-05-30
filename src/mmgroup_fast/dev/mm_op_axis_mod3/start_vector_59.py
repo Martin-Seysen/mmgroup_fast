153c4e1c11fa940d337eb73d6e948ae096eb7c6c
Dr. Martin Seysen <m.seysen@gmx.de>
2026-05-31 00:10:14 +0200

    Changes in file start_vector_59.py

--- src/mmgroup_fast/dev/mm_op_axis_mod3/start_vector_59.py
+++ src/mmgroup_fast/dev/mm_op_axis_mod3/start_vector_59.py
@@ -7,8 +7,9 @@ MY_PY_PATH = os.path.abspath(os.path.join('..', '..', '..'))
 _DIR = os.path.split(__file__)[0]
 PY_FILENAME = os.path.join(_DIR, "order_vector_data.py")
 
-from mmgroup import MM, MMVector
+from mmgroup import MM, MM0, MMVector
 from mmgroup.dev.mm_reduce.find_order_vector import find_element_of_order
+from mmgroup.dev.mm_reduce.find_order_vector import find_vector_p_mod3
 
 ####################################################################
 ## Compute vector v59 and order vector s59 from group element g59
@@ -22,7 +23,20 @@ def str_data(text, data):
     s += "\n]\n"
     return s
     
-def s59_from_v59(g59, v59):
+def s59_from_v59(g59, v59, a59):
+    r"""Compute a vector stabilized by a group element of order 59
+
+    The function returns a vector ``s59``  in the representation of
+    the  monster modulo 3, such that the vector
+    ``s59 = sum(v59 * g59**i for i in range(59)) * a59`` is not
+    trivial. Then ``s59`` is stabilized by ``g59 ** a59``. More
+    precisely, the projection of ``s59`` onto the 196883-dimensional
+    irreducible representation of the monster is not trivial.
+
+    Here ``g59`` and ``a59`` are instances of class ``MM``
+    (or ``MM0``); and  ``v59`` and the return value``s59`` are
+    vectors which are instances of  class ``MMVector``.
+    """
     v = v59.copy()
     s59 = v.copy()
     for j in range(58):
@@ -30,37 +44,13 @@ def s59_from_v59(g59, v59):
         s59 += v
     assert v59 * g59 != v59
     assert s59 * g59 == s59
-    for j in range(24):
-         if s59["X", 0, j]:
-             return g59, v59, s59
-    raise ValueError("Bad vector pair g59, v59")
+    s59 *= a59
+    for i in range(10):
+        for j in range(24):
+            if s59["X", 0, j]:
+                return s59
+    raise ValueError("Bad vector tuple g59, v59, a59")
 
-def s59_from_g59(g59, v59=None):
-    r"""Compute a vector stabilized by a group element of order 59
-
-    The function takes an element ``g59`` of order 59 of the
-    monster and a vector ``v59``  in the representation of the
-    monster modulo 3, such that the vector
-    ``s59 = sum(v59 * g59**i for i in range(59))`` is not trivial.
-    Then ``s59`` is stabilized by ``g59``. More precisely, the
-    projection of ``s59`` onto the 196883-dimensional irreducible
-    representation of the monster is not trivial.
-
-    The function returns the triple ``(g59, v59, s59)``.  Here
-    ``g59`` and ``s59`` are instances of class ``MM``, and ``s59``
-    is a vector which is an intance of  class ``MMVector``.
-
-    If input ``v59`` is ``None``, a random vector is generated.
-    """
-    if v59:
-        return s59_from_v59(g59, v59)
-    for i in range(24):
-        try:
-            v59 = MMVector(3, "X", 0, i)
-            return s59_from_v59(g59, v59)
-        except:
-            continue
-    raise ValueError("Bad vector g59")
 
 ####################################################################
 ## Write order vector data to file
@@ -71,17 +61,16 @@ HEADER = """# This file has been created automatically, do not change!
 # mmgroup.dev.mm_reduce.find_order_vector.start_vector_59.py.
 """
 
-def write_vector_59_mod3(g59, v59):
+def write_vector_59_mod3(s_g, s_v, s_gA):
     """Write group element g59 and vector v59 to file"""
+    a_g = MM0(s_g).mmdata
+    a_v = MMVector(3,s_v).as_sparse()
+    a_gA = MM0(s_gA).mmdata
     print("Writing file " + PY_FILENAME)
-    assert isinstance(g59, MM)
-    assert isinstance(v59, MMVector) and v59.p == 3
     f = open(PY_FILENAME, "wt")
     print(HEADER, file = f)
-    a_v59 = v59.as_sparse()
-    a_g59 = g59.mmdata
-    for text, data in [("G59", a_g59), ("V59", a_v59)]:
-         print(str_data(text, data), file = f)
+    for text, data in [("G59", a_g), ("V59", a_v), ("A59", a_gA)]:
+        print(str_data(text, data), file = f)
     f.close()
 
 ####################################################################
@@ -89,6 +78,33 @@ def write_vector_59_mod3(g59, v59):
 ####################################################################
 
 def read_vector_59_mod3(recompute = False, verbose = 0):
+    r"""Compute a vector stabilized by a group element of order 59
+
+
+    The function returns the tuple ``(g59, v59, a59)``.  Here
+    ``g59`` and ``a59`` are instances of class ``MM``; and ``s59``
+    is a vector which is an instance of  class ``MMVector``.
+
+    `These data are read from a file if possible.
+    ``recompute = True`` forces recomputation of these values.
+    """
+    process_args = [sys.executable, "start_vector_59.py", "-w"]
+    sys.path.append(MY_PY_PATH)
+    import mmgroup_fast
+    try:
+        assert not recompute
+        from mmgroup_fast.dev.mm_op_axis_mod3.order_vector_data import G59, V59, A59
+    except:
+        subprocess.check_call(process_args, cwd=_DIR)
+        from mmgroup_fast.dev.mm_op_axis_mod3.order_vector_data import G59, V59, A59
+    sys.path.pop()
+    g59 = MM('a', G59)
+    v59 = MMVector(3, 'S', V59)
+    a59 = MM('a', A59)
+    return g59, v59, a59
+
+
+def compute_vector_59_mod3(g59, v59, a59):
     r"""Compute a vector stabilized by a group element of order 59
 
     The function computes an element ``g59`` of order 59 of the
@@ -111,14 +127,18 @@ def read_vector_59_mod3(recompute = False, verbose = 0):
     import mmgroup_fast
     try:
         assert not recompute
-        from mmgroup_fast.dev.mm_op_axis_mod3.order_vector_data import G59, V59
+        from mmgroup_fast.dev.mm_op_axis_mod3.order_vector_data import G59, V59, A59
     except:
         subprocess.check_call(process_args, cwd=_DIR)
-        from mmgroup_fast.dev.mm_op_axis_mod3.order_vector_data import G59, V59
+        from mmgroup_fast.dev.mm_op_axis_mod3.order_vector_data import G59, V59, A59
     sys.path.pop()
     g59 = MM('a', G59)
     v59 = MMVector(3, 'S', V59)
-    return s59_from_g59(g59, v59)
+    a59 = MM('a', G59)
+    return s59_from_g59(g59, v59, a59)
+
+
+
 
 ####################################################################
 ## Concatenate components of vector
@@ -152,7 +172,8 @@ def concatenate_vector(v, tags, verbose=0):
 class Tables:
     directives = {}
     def __init__(self):
-        s59 = read_vector_59_mod3()[2]
+        g59, v59, a59 = read_vector_59_mod3()
+        s59 = s59_from_v59(g59, v59, a59)
         sABC = concatenate_vector(s59, "ABC")
         sT = concatenate_vector(s59, "T")
         sXZY = concatenate_vector(s59, "XZY")
@@ -183,17 +204,22 @@ if __name__ == "__main__":
     if "-w" in sys.argv:
         for i in range(10):
             try:
-                s_g59 = find_element_of_order(59)
-                g59, v59, s59 = s59_from_g59(MM(s_g59))
+                s_g, s_v, s_gA = find_vector_p_mod3(59)
                 break
             except:
                 #raise
                 continue
         try:
-            write_vector_59_mod3(g59, v59)
+            print(s_g, s_v, s_gA)
+            write_vector_59_mod3(s_g, s_v, s_gA)
         except:
             raise ValueError("No order vector found")
+    g59, v59, a59 = read_vector_59_mod3(recompute=False, verbose=1)
+    s59 = s59_from_v59(g59, v59, a59)
     if "-v" in sys.argv:
-        g59, v59, s59 = read_vector_59_mod3(recompute=False, verbose=1)
         print("g59 =", g59)
         print("v59 =", v59)
+        print("a59 =", a59)
+
+
+
--- src/mmgroup_fast/tests/test_amod3/test_order_vector.py
+++ src/mmgroup_fast/tests/test_amod3/test_order_vector.py
@@ -6,6 +6,7 @@ import pytest
 from mmgroup import MMV, MM0, MM, MMSpace, Xsp2_Co1, XLeech2
 from mmgroup_fast.mm_op_fast import MMOpFastMatrix
 from mmgroup_fast.dev.mm_op_axis_mod3.start_vector_59 import read_vector_59_mod3
+from mmgroup_fast.dev.mm_op_axis_mod3.start_vector_59 import s59_from_v59
 
 
 MMV3 = MMV(3)
@@ -16,7 +17,8 @@ def test_order_vector():
     m = MMOpFastMatrix(3, 4, 1)
     m.set_vstd(hash=1)
     ov = m.row_as_mmv(3)
-    ov_ref = read_vector_59_mod3()[2]
+    g59, v59, a59 = read_vector_59_mod3()
+    ov_ref = s59_from_v59(g59, v59, a59)
     #for tag in "ABCTXZY":
     #    print(tag)
     #    assert (ov[tag] == ov_ref[tag]).all()
